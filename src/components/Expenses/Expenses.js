a2b813a4a7a0d1abf9edad21b3227e566174ac26
chefavocado <85806647+chefavocado@users.noreply.github.com>
2022-03-01 21:42:42 -0500

    Module 5 Completed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -23,10 +24,11 @@ const Expenses = (props) => {
           selectedYear={filteredYear}
           onYearChange={saveYearHandler}
         />
-        <ExpensesList items={filteredExpenses}/>
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
-};
+}; 
 
 export default Expenses;
